f92746bf02edbfb7d101eae82b6ed4c2873e724f
Akbar Maliki Haqoni Jati <akbar.maliki@ui.ac.id>
2023-08-14 09:06:29 +0700

    change if to assert

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -6,15 +6,25 @@ import (
 	"net"
 	"testing"
 
+	"amitshekar-clean-arch/bootstrap"
+	repository "amitshekar-clean-arch/repository/mysql"
 	pb "amitshekar-clean-arch/todogrpc"
+	"amitshekar-clean-arch/usecase"
 
+	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/test/bufconn"
 )
 
 func TestTodoUsecase_GetTodo(t *testing.T) {
 	// ---- Server Initialization ----
+	app = bootstrap.App()
 
+	db := app.MySql
+
+	tr = repository.NewMysqlTodoRepo(db)
+	tu = usecase.NewTodoUsecase(tr)
 	// setup a listener
 	lis := bufconn.Listen(1024 * 1024)
 	// setup defer to clean
@@ -41,7 +51,7 @@ func TestTodoUsecase_GetTodo(t *testing.T) {
 	dialer := func(context.Context, string) (net.Conn, error) {
 		return lis.Dial()
 	}
-	conn, err := grpc.DialContext(context.Background(), "", grpc.WithContextDialer(dialer), grpc.WithInsecure())
+	conn, err := grpc.DialContext(context.Background(), "", grpc.WithContextDialer(dialer), grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	t.Cleanup(func() {
 		conn.Close()
@@ -53,13 +63,28 @@ func TestTodoUsecase_GetTodo(t *testing.T) {
 
 	client := pb.NewTodoCRUDClient(conn)
 	taskName := "Task 1"
-	res, err := client.GetTodo(context.Background(), &pb.TodoName{Name: taskName})
-
-	if err != nil {
-		t.Fatalf("client.gettodo %v", err)
+	todoStatus := "Done"
+	todo := pb.NewTodo{
+		Name:   taskName,
+		Status: todoStatus,
+	}
+	todoUpdate := pb.NewTodo{
+		Name:   taskName,
+		Status: "Not Done",
 	}
 
-	if res.Name != "Task 1" {
-		t.Fatal("Values that is returned is error!")
-	}
+	pbErrorCreate, _ := client.CreateTodo(context.Background(), &todo)
+	assert.Equal(t, "Success!", pbErrorCreate.Message)
+
+	pbErrorUpdate, _ := client.UpdateTodo(context.Background(), &todoUpdate)
+	assert.Equal(t, "Success!", pbErrorUpdate.Message)
+
+	pbTodo, err := client.GetTodo(context.Background(), &pb.TodoName{Name: taskName})
+	assert.ErrorIs(t, nil, err)
+	assert.Equal(t, "Task 1", pbTodo.Name)
+	// Test status update
+	assert.Equal(t, todoUpdate.Status, pbTodo.Status)
+
+	pbErrorDelete, _ := client.DeleteTodo(context.Background(), &pb.TodoName{Name: taskName})
+	assert.Equal(t, "Success!", pbErrorDelete.Message)
 }
